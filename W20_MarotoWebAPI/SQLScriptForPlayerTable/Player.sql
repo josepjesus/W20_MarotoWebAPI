8f1fd1bc8aca34dd9365af661fb3222358c94824
josepjesus <pepjesus@hotmail.es>
2020-02-21 19:08:32 +0100

    first commit

---
+++ W20_MarotoWebAPI/SQLScriptForPlayerTable/Player.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Players] (
+    [Id]       NVARCHAR (128) NOT NULL,
+    [Name]     NVARCHAR (256) NOT NULL,
+    [Email]    NVARCHAR (256) NOT NULL,
+    [BirthDay] DATETIME2 (7)  NOT NULL,
+    [isLoged] TINYINT NOT NULL DEFAULT 0, 
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Players_To_AspNetUsers] FOREIGN KEY ([Id]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
